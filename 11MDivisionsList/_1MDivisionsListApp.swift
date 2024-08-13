f2f7eb044d071d9929442f71f9661a58ef82a1fa
AndyC <andy@ancl.org>
2024-08-13 10:04:46 +0100

    Initial commit

---
+++ 11MDivisionsList/_1MDivisionsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1MDivisionsListApp.swift
+//  11MDivisionsList
+//
+//  Created by Andrew Clements on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct _1MDivisionsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
